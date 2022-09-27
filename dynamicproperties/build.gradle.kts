5359870580ed9a8d09261fea2722efc975b7d8f5
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-09-27 19:36:07 -0300

    setting gradle

---
+++ dynamicproperties/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+}
+
+group = "tk.leonardocavazzola"
+version = "1.0.0"
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.projectlombok:lombok:1.18.22")
+    annotationProcessor("org.projectlombok:lombok:1.18.22")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/LeonardoCavazzola/Utils")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
---
+++ security/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+}
+
+group = "tk.leonardocavazzola"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/LeonardoCavazzola/Utils")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
