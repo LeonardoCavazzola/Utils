939844d2df32dd6bcb896e2450828ece39ce102f
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-09-26 23:06:30 -0300

    add dotenv scanner

---
+++ dotenv/src/main/java/utils/dotenv/DotenvScanner.java
@@ -0,0 +1,34 @@
+package utils.dotenv;
+
+import lombok.extern.java.Log;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+@Log
+public class DotenvScanner {
+    public static void scanProperties(String path) throws IOException {
+        try {
+            Properties properties = readFile(path);
+            System.setProperties(properties);
+        } catch (FileNotFoundException ex) {
+            log.warning("File not found");
+        }
+        log.info(".env properties has been scanned");
+    }
+
+    private static Properties readFile(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        Properties properties = new Properties();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] kv = line.split("=");
+            properties.put(kv[0], kv[1]);
+        }
+        reader.close();
+        return properties;
+    }
+}
